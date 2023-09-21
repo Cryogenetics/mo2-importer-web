b3ae83321a459105d499ddcb2138e22910727b1d
Cryogenetics <kyskiddie@protonmail.com>
2023-09-21 11:39:22 -0500

    Update [[url]].js
    make more readable

--- functions/bypassCors/[[url]].js
+++ functions/bypassCors/[[url]].js
@@ -1,12 +1,13 @@
 export async function onRequest({ request }) {
     let rURL = new URL(request.url);
-    console.log(rURL);
     const sid_develop = request.headers.get("Authorization");
     let newUrl = "https://" + request.url.replace(rURL.origin + "/bypassCors/", "");
-    console.log(newUrl);
     let Req = new Request(newUrl, request);
     Req.headers.has("origin") && Req.headers.delete("origin");
     Req.headers.has("referer") && Req.headers.delete("referer");
-    let n = await fetch(Req, { headers: { Cookie: `sid_develop=${sid_develop}` } });
-    return n = new Response(n.body, n), n.headers.set("access-control-allow-origin", "*"), n.headers.set("access-control-allow-headers", "*"), n;
+    let res = await fetch(Req, { headers: { Cookie: `sid_develop=${sid_develop}` } });
+    let newRes = new Response(res.body,res)
+    newRes.headers.set("access-control-allow-origin", "*")
+    newRes.headers.set("access-control-allow-headers", "*");
+    return newRes;
 }
